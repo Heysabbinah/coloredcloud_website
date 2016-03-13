46d17f78e5a87016aeb8cf9a66a8121b76a87f37
heysabbinah <dubysound@hotmail.com>
2016-03-13 14:34:55 +0100

    add some css style for responsive design for project pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static#index'
   get 'projet' => 'static#projets'
+  get 'oxyghem' => 'static#oxyghem'
+  get 'simplon' => 'static#simplon'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
